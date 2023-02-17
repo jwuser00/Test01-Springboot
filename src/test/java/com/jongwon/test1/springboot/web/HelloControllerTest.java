4a7a52c2d1e2ffc524b1969a200243272415d413
jwuser00 <jwuser00@gmail.com>
2023-02-18 01:02:03 +0900

    login - testing

--- src/test/java/com/jongwon/test1/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jongwon/test1/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.jongwon.test1.springboot.web;
 
 
+import com.jongwon.test1.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,11 +17,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest( controllers = HelloController.class,
+    excludeFilters = { @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)} )
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_return() throws Exception {
         String hello = "hello";
@@ -27,6 +33,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void TEST_helloDto() throws Exception {
         String name = "hello";

1175558bcfc7491d79dbdd4db233dc0e842e5010
jwuser00 <jwuser00@gmail.com>
2023-02-17 23:24:46 +0900

    add update / get

--- src/main/java/com/jongwon/test1/springboot/web/IndexController.java
+++ src/main/java/com/jongwon/test1/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.jongwon.test1.springboot.web;
 
+import com.jongwon.test1.springboot.config.auth.LoginUser;
+import com.jongwon.test1.springboot.config.auth.dto.SessionUser;
+import com.jongwon.test1.springboot.domain.user.User;
 import com.jongwon.test1.springboot.service.PostsService;
 import com.jongwon.test1.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +12,24 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //SessionUser user = (SessionUser)(httpSession.getAttribute("user"));
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
